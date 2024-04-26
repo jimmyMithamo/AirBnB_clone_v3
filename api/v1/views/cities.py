320dcfcb444ae2af33b45cc2e3a2e19069c0624e
jimmyMithamo <j1mithamo@gmail.com>
2024-04-27 00:14:05 +0300

    cities view

---
+++ api/v1/views/cities.py
@@ -0,0 +1,19 @@
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+def get_cities(state_id):
+    """return all cities in a state"""
+    cities = storage.all(City).values()
+    return jsonify([city.to_dict() for city in cities if city.state_id == state_id])
+
+@app_views.route("cities/<city_id>", methods=["GET"], strict_slashes=False)
+def get_city(city_id):
+    """retrieves a city by id"""
+    city = storage.get(City, city_id)
+    if city:
+        return jsonify(city.to_dict())
+    else:
+        abort(404)
