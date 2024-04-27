83812f6e33272a6f0dadbcea2ff31878d69670da
Soid23 <dorrismutahi@gmail.com>
2024-04-27 06:27:25 +0300

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """
-index.py
+index
 """
 
 from flask import jsonify
+from api.v1.views import app_views
+
 from models import storage
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    """retrieves statistics about the objects"""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
     }
-    return jsonify(stats)
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
