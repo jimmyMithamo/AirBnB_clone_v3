7848e3250a9700b7ef0dca0b6af4efa00508a01c
Soid23 <dorrismutahi@gmail.com>
2024-04-27 15:53:10 +0300

    users.py

---
+++ api/v1/views/users.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+user.api
+"""
+
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+
+
+@app_views.route("/users/", methods=["GET"])
+def get_users():
+    """Obtaining all the Users"""
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return (jsonify(users)), 200
+
+
+@app_views.route("/users/<user_id>", methods=["GET"])
+def get_user(user_id):
+    """Obtaining a specific User"""
+    if storage.get(User, user_id) is not None:
+        user = storage.get(User, user_id)
+        return jsonify(user.to_dict()), 200
+    else:
+        abort(404)
+
+
+@app_views.route("/users/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    """Deleting a User"""
+    if storage.get(User, user_id) is not None:
+        user = storage.get(User, user_id)
+        storage.delete(user)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        abort(404)
+
+
+@app_views.route("/users/", methods=["POST"])
+def create_user():
+    """Creating a User"""
+    if not request.is_json:
+        abort(400, "Not a JSON")
+    if 'email' not in request.get_json():
+        abort(400, "Missing email")
+    if 'password' not in request.get_json():
+        abort(400, "Missing password")
+    new_user = User(email=request.json['email'],
+                    password=request.json['password'])
+    storage.new(new_user)
+    storage.save()
+    user = new_user.to_dict()
+    return jsonify(user), 201
+
+
+@app_views.route("/users/<user_id>", methods=["PUT"])
+def edit_user(user_id):
+    """Editing a User"""
+    if not request.is_json:
+        abort(400, "Not a JSON")
+    if storage.get(User, user_id) is not None:
+        user = storage.get(User, user_id)
+        if 'first_name' in request.get_json():
+            user.first_name = request.json['first_name']
+        if 'last_name' in request.get_json():
+            user.last_name = request.json['last_name']
+        if 'password' in request.get_json():
+            user.password = request.json['password']
+        storage.save()
+        user = storage.get(User, user_id)
+        return jsonify(user.to_dict()), 200
+    else:
+        abort(404)
