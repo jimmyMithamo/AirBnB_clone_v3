202ff83c3a6a0cce67362eb56ce0f6f917917807
Soid23 <dorrismutahi@gmail.com>
2024-04-27 13:36:08 +0300

    status index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,13 @@ index.py
 
 from flask import jsonify
 from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_status():
+    """ Returns the status of the api."""
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
