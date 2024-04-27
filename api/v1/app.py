85588214970b0096ccfc70e05608baf0fb03496a
Soid23 <dorrismutahi@gmail.com>
2024-04-27 13:45:57 +0300

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,4 +29,4 @@ def error_handler(error):
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
-    app.run(host=host, port=port, debug=True, threaded=True)
+    app.run(host=host, port=port, debug=False, threaded=True)
