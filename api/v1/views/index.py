2648059829c3d112e24e593e36b5660171ee5630
jimmyMithamo <j1mithamo@gmail.com>
2024-04-28 14:30:19 +0300

    files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,10 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""
-index.py
-"""
-
-from flask import jsonify
-from models import storage
-from api.v1.views import app_views
-
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def get_status():
-    """ Returns the status of the api."""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-=======
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
 @app_views.route('/stats', methods=['GET'])
->>>>>>> 7e28e44 (changes)
 def get_stats():
     """retrieves statistics about the objects"""
     stats = {
@@ -34,7 +15,4 @@ def get_stats():
         "users": storage.count("User")
     }
     return jsonify(stats)
-<<<<<<< HEAD
-=======
 
->>>>>>> 7e28e44 (changes)
