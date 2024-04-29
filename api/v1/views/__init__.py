010ffe88cdb619602e6796130c2dc10cd12c36fe
jimmyMithamo <j1mithamo@gmail.com>
2024-04-29 10:52:11 +0300

    changes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,25 +6,10 @@ creating a blueprint
 from flask import Blueprint
 from .index import *
 from .state import *
+from api.v1.views.index import app_views
 
 """
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 """
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-#!/usr/bin/python3
-"""
-creating a blueprint
-"""
-
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.places import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
