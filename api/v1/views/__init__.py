05464c05989bbbb4627770cdf9fa3afecab513c0
jimmyMithamo <j1mithamo@gmail.com>
2024-04-29 12:05:07 +0300

    checks

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,10 +4,8 @@ creating a blueprint
 """
 
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
 from .index import *
 from .states import *
 from api.v1.views.index import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
