27987c1edd53679464d5555dab01cac4d06e1ce8
Soid23 <dorrismutahi@gmail.com>
2024-05-03 23:07:28 +0300

    inedex

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,13 +4,14 @@ from models import storage
 from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route('/status', strict_slashes = False)
+
+@app_views.route('/status', strict_slashes=False)
 def status():
     """returns status of our api"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes = False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stat():
     """gets the number of class objects"""
     new_dict = {
